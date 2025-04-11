b6e97b04bdb51bf3e78b63a5e7aa58833824d906
jbdoy <jennifer.bendoy@cit.edu>
2025-04-11 21:18:44 +0800

    Teacher Registration

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import StudentLogin from './student/StudentLogin';
 import TeacherLogin from './teacher/TeacherLogin';
 import StudentRegistration from './student/StudentRegistration';
+import TeacherRegistration from './teacher/TeacherRegistration';
 import StudentHomepage from './student/StudentHomepage';
+import TeacherHomepage from './teacher/TeacherHomepage';
+
 
 const App = () => {
   return (
@@ -12,7 +15,9 @@ const App = () => {
       <Route path="/student-login" element={<StudentLogin />} />
       <Route path="/teacher-login" element={<TeacherLogin />} />
       <Route path="/student-register" element={<StudentRegistration />} />
+      <Route path="/teacher-register" element={<TeacherRegistration />} />
       <Route path="/student-homepage" element={<StudentHomepage />} />
+      <Route path="/teacher-homepage" element={<TeacherHomepage />} />
     </Routes>
   );
 };
---
+++ src/teacher/TeacherRegistration.jsx
@@ -0,0 +1,156 @@
+import React, { useState } from 'react';
+import { Button, TextField, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import bookImage from '../assets/book.png';
+
+const TeacherRegistration = () => {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email: '',
+    firstName: '',
+    lastName: '',
+    teacherId: '',
+    password: '',
+    verifyPassword: '',
+    businessCode: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleRegister = () => {
+    console.log('Registering teacher:', formData);
+    navigate("/teacher-login");
+  };
+
+  return (
+    <div style={{ 
+      backgroundColor: '#FFFBE0', 
+      height: '100vh', 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center',
+      position: 'relative',
+    }}>
+      <div style={{ 
+        textAlign: 'center',
+        width: '350px',
+        padding: '20px',
+        borderRadius: '10px'
+      }}>
+        <img src={bookImage} alt="Books" style={{ 
+          position: 'absolute', 
+          bottom: '0px', 
+          right: '0px', 
+          width: '300px' 
+        }} />
+        
+        <Typography variant="h3" style={{ fontWeight: 'bold', marginBottom: '10px' }}>
+          SCRIBBIE
+        </Typography>
+
+        <TextField
+          name="email"
+          label="Email"
+          size="small"
+          value={formData.email}
+          onChange={handleChange}
+          fullWidth
+          margin="dense"
+          variant="outlined"
+          style={{ backgroundColor: '#FFDE9A' }}
+        />
+
+        <TextField
+          name="firstName"
+          label="First Name"
+          size="small"
+          value={formData.firstName}
+          onChange={handleChange}
+          fullWidth
+          margin="dense"
+          variant="outlined"
+          style={{ backgroundColor: '#FFDE9A' }}
+        />
+
+        <TextField
+          name="lastName"
+          label="Last Name"
+          size="small"
+          value={formData.lastName}
+          onChange={handleChange}
+          fullWidth
+          margin="dense"
+          variant="outlined"
+          style={{ backgroundColor: '#FFDE9A' }}
+        />
+
+        <TextField
+          name="teacherId"
+          label="Teacher ID"
+          size="small"
+          value={formData.teacherId}
+          onChange={handleChange}
+          fullWidth
+          margin="dense"
+          variant="outlined"
+          style={{ backgroundColor: '#FFDE9A' }}
+        />
+
+        <TextField
+          name="password"
+          label="Password"
+          type="password"
+          size="small"
+          value={formData.password}
+          onChange={handleChange}
+          fullWidth
+          margin="dense"
+          variant="outlined"
+          style={{ backgroundColor: '#FFDE9A' }}
+        />
+
+        <TextField
+          name="verifyPassword"
+          label="Verify Password"
+          type="password"
+          size="small"
+          value={formData.verifyPassword}
+          onChange={handleChange}
+          fullWidth
+          margin="dense"
+          variant="outlined"
+          style={{ backgroundColor: '#FFDE9A' }}
+        />
+
+        <TextField
+          name="businessCode"
+          label="Business Code"
+          size="small"
+          value={formData.businessCode}
+          onChange={handleChange}
+          fullWidth
+          margin="dense"
+          variant="outlined"
+          style={{ backgroundColor: '#FFDE9A' }}
+        />
+
+        <Button 
+          onClick={handleRegister} 
+          style={{ 
+            backgroundColor: '#451513', 
+            color: 'white', 
+            marginTop: '20px', 
+            width: '100%' 
+          }}
+        >
+          Register
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default TeacherRegistration;
