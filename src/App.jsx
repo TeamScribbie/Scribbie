73efeef55bed35cb4e4c9f22a28875eb9816c1c8
Avordia <aesopianproductions@gmail.com>
2025-04-13 20:30:47 +0800

    Merge StudentAndTeacher

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,22 @@
-<<<<<<< HEAD
 import { Routes, Route } from "react-router-dom";
-import NotFound from "./page/NotFound";
 import StudentHomepage from "./page/student/StudentHomepage";
 import StudentLogin from "./page/student/StudentLogin";
 import StudentRegistration from "./page/student/StudentRegistration";
-=======
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import TeacherLogin from './page/teacher/TeacherLogin';
 import TeacherRegistration from './page/teacher/TeacherRegistration';
 import TeacherHomepage from './page/teacher/TeacherHomepage';
->>>>>>> teacher
 
 const App = () => {
   return (
     <Routes>
-<<<<<<< HEAD
       <Route path="/student-homepage" element={<StudentHomepage />} />
       <Route path="/" element={<StudentLogin />} />
       <Route path="/student-register" element={<StudentRegistration />} />
-      <Route path="*" element={<NotFound />} />
-=======
-      <Route path="/" element={<Navigate to="/teacher-login" replace />} />
       <Route path="/teacher-login" element={<TeacherLogin />} />
       <Route path="/teacher-register" element={<TeacherRegistration />} />
       <Route path="/teacher-homepage" element={<TeacherHomepage />} />
->>>>>>> teacher
     </Routes>
   );
 };
