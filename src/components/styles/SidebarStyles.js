a25512d90e876537a3d6c2e4f7e9168359b32fe3
jbdoy <jennifer.bendoy@cit.edu>
2025-04-22 19:06:34 +0800

    style for the sidebar

---
+++ src/components/styles/SidebarStyles.js
@@ -0,0 +1,37 @@
+const SidebarStyles = {
+    sidebar: {
+      height: "100vh",
+      backgroundColor: "#FFF4CC",
+      position: "fixed",
+      top: 0,
+      left: 0,
+    },
+    sidebarTitle: {
+      fontWeight: "bold",
+      marginBottom: "30px",
+      color: "#451513",
+    },
+    sidebarItem: {
+      padding: "10px 15px",
+      marginBottom: "10px",
+      cursor: "pointer",
+      fontWeight: "800",
+      backgroundColor: "#fff",
+      color: "#451513",
+      borderRadius: "15px",
+      transition: "0.2s",
+    },
+    subItem: {
+      paddingLeft: "20px",
+      paddingTop: "5px",
+      paddingBottom: "5px",
+      fontSize: "0.9rem",
+      color: "#555",
+    },
+    classList: {
+      marginTop: "10px",
+    },
+  };
+  
+  export default SidebarStyles;
+  
