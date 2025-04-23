2f2e52cdb3e390ce838fe668c368e3b38beddb1d
Avordia <aesopianproductions@gmail.com>
2025-04-24 00:03:52 +0800

    AuthProvider tweak

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+// src/App.jsx (Corrected Code)
 import React from 'react';
-import Navbar from "./components/layout/navbar";
+import Navbar from "./components/layout/navbar"; // Assuming Navbar might also use auth context later
 import StudentHomepage from "./page/student/StudentHomepage";
 import StudentLogin from "./page/student/StudentLogin";
 import StudentRegistration from "./page/student/StudentRegistration";
@@ -8,21 +9,37 @@ import TeacherLogin from './page/teacher/TeacherLogin';
 import TeacherRegistration from './page/teacher/TeacherRegistration';
 import TeacherHomepage from './page/teacher/TeacherHomepage';
 
+// 1. Import AuthProvider
+import { AuthProvider } from './context/AuthContext';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/nav" element={<Navbar />} />
-      <Route path="/student-homepage" element={<StudentHomepage />} />
-      <Route path="/student-homepage" element={<StudentHomepage />} />
-      <Route path="/student-login" element={<StudentLogin />} />
-      <Route path="/" element={<StudentLogin />} />
-      <Route path="/student-register" element={<StudentRegistration />} />
-      <Route path="/" element={<Navigate to="/teacher-login" replace />} />
-      <Route path="/teacher-login" element={<TeacherLogin />} />
-      <Route path="/teacher-register" element={<TeacherRegistration />} />
-      <Route path="/teacher-homepage" element={<TeacherHomepage />} />
-    </Routes>
+    // 2. Wrap Routes with AuthProvider
+    <AuthProvider>
+      <Routes>
+        {/* Navbar might eventually be inside specific layouts or routes needing auth */}
+        {/* <Route path="/nav" element={<Navbar />} /> */}
+
+        {/* Define routes that DO NOT need auth first (like login/register) */}
+        <Route path="/student-login" element={<StudentLogin />} />
+        <Route path="/student-register" element={<StudentRegistration />} />
+        <Route path="/teacher-login" element={<TeacherLogin />} />
+        <Route path="/teacher-register" element={<TeacherRegistration />} />
+
+        {/* Define routes that DO need auth */}
+        {/* You might want to wrap these in a ProtectedRoute component later */}
+        <Route path="/student-homepage" element={<StudentHomepage />} />
+        <Route path="/teacher-homepage" element={<TeacherHomepage />} />
+
+
+        {/* Default route redirects */}
+        {/* Consider redirecting based on auth status later */}
+        <Route path="/" element={<Navigate to="/student-login" replace />} />
+
+        {/* Add other routes as needed */}
+
+      </Routes>
+    </AuthProvider>
   );
 };
 
