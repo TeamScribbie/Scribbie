bb9af2f40410407d94259c1278df918b7e929397
jbdoy <jennifer.bendoy@cit.edu>
2025-04-29 22:29:41 +0800

    adjust leaderboardbox layout

--- src/page/student/LeaderboardBox.jsx
+++ src/page/student/LeaderboardBox.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-
+import Navbar from "../../components/layout/TeacherNavbar";
 const LeaderboardBox = () => {
   const [scores, setScores] = useState([]);
 
