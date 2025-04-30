a0b50dd647785f75f99d5c8fb17f32c0e6feb0fd
jbdoy <jennifer.bendoy@cit.edu>
2025-04-30 17:10:52 +0800

    new changes

--- src/page/student/ChallengeListPage.jsx
+++ src/page/student/ChallengeListPage.jsx
@@ -13,6 +13,7 @@ const ChallengeListPage = () => {
   const challenges = [
     { id: 1, title: "Challenge 1" },
     { id: 2, title: "Challenge 2" },
+    { id: 3, title: "Challenge 3" },
   ];
 
   useEffect(() => {
