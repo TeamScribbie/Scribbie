5338833bbdf597620299efd6d285e58e07f8f90c
Avordia <aesopianproductions@gmail.com>
2025-07-08 22:59:16 +0800

    Edit logs

--- src/components/dialogs/AddActivityNodeDialog.jsx
+++ src/components/dialogs/AddActivityNodeDialog.jsx
@@ -7,7 +7,7 @@ import {
 } from '@mui/material';
 
 const ACTIVITY_TYPES = [
-    "MATCHING", "FILL_BLANKS", "READING", "MATCHING2"
+    "MATCHING", "FILL_BLANKS", "READING", "MATCHING2", "BALLOONGAME", "MEMORYGAME"
 ];
 
 const AddActivityNodeDialog = ({ open, onClose, onConfirm, isLoading, error }) => {
