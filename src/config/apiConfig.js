4ff3fae3399f4079edee635fd94e73ea56cd9ab5
Avordia <aesopianproductions@gmail.com>
2025-09-27 17:42:26 +0800

    API fix

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -6,8 +6,8 @@
 // export const MEDIA_BASE_URL = 'http://localhost:8080/';
 
 //FOR DEPLOYMENT
-export const API_BASE_URL = 'https://scribbiealpha.serveblog.net';
+export const API_BASE_URL = 'https://scribbiealpha.serveblog.net/api';
 export const MEDIA_BASE_URL = 'https://scribbiealpha.serveblog.net';
-export const API_TIMEOUT = 30000; 
+export const API_TIMEOUT = 30000;
 
 
