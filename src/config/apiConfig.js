e6253a25f7f4d0ef86c93ecd08f0eed68bd6ccb6
Avordia <aesopianproductions@gmail.com>
2025-09-27 13:49:15 +0800

    Challenge Buttons

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,9 +1,9 @@
 //PARA INYOHA NI
-// export const API_BASE_URL = 'http://152.42.254.129:8080/api';
-// export const MEDIA_BASE_URL = 'http://152.42.254.129:8080/';
+export const API_BASE_URL = 'http://152.42.254.129:8080/api';
+export const MEDIA_BASE_URL = 'http://152.42.254.129:8080/';
 //FOR BACKEND DEVELOPERS NI
-export const API_BASE_URL = 'http://localhost:8080/api';
-export const MEDIA_BASE_URL = 'http://localhost:8080/';
+// export const API_BASE_URL = 'http://localhost:8080/api';
+// export const MEDIA_BASE_URL = 'http://localhost:8080/';
 export const API_TIMEOUT = 30000; 
 
 
