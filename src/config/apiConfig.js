5edde7cd1a15055892ef66613c33ca7323e37d4b
Avordia <aesopianproductions@gmail.com>
2025-09-30 17:38:08 +0800

    Api-Change

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -2,13 +2,21 @@
 // export const API_BASE_URL = 'http://152.42.254.129:8080/api';
 // export const MEDIA_BASE_URL = 'http://152.42.254.129:8080/';
 
+
 //FOR BACKEND DEVELOPERS NI
-export const API_BASE_URL = 'http://localhost:8080/api';
-export const MEDIA_BASE_URL = 'http://localhost:8080/';
+// export const API_BASE_URL = 'http://localhost:8080/api';
+// export const MEDIA_BASE_URL = 'http://localhost:8080/';
 
 //FOR DEPLOYMENT
 // export const API_BASE_URL = 'https://scribbiealpha.serveblog.net/api';
 // export const MEDIA_BASE_URL = 'https://scribbiealpha.serveblog.net';
+
+// export const API_BASE_URL = 'https://scribbiealpha.serveblog.net/api';
+// export const MEDIA_BASE_URL = 'https://scribbiealpha.serveblog.net';
+
+export const API_BASE_URL = 'https://trialrender-production.up.railway.app/api';
+export const MEDIA_BASE_URL = 'https://trialrender-production.up.railway.app/';
+
 export const API_TIMEOUT = 30000;
 
 
