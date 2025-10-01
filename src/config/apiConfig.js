6f0fe45231f6f6369f12952d643fdd2ad6b38bee
Avordia <aesopianproductions@gmail.com>
2025-10-01 15:36:02 +0800

    Student Sidebar updates

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 //PARA INYOHA NI
-export const API_BASE_URL = 'http://152.42.254.129:8080/api';
-export const MEDIA_BASE_URL = 'http://152.42.254.129:8080/';
+// export const API_BASE_URL = 'http://152.42.254.129:8080/api';
+// export const MEDIA_BASE_URL = 'http://152.42.254.129:8080/';
 
 
 //FOR BACKEND DEVELOPERS NI
@@ -8,8 +8,8 @@ export const MEDIA_BASE_URL = 'http://152.42.254.129:8080/';
 //export const MEDIA_BASE_URL = 'http://localhost:8080/';
 
 //FOR DEPLOYMENT
-// export const API_BASE_URL = 'https://scribbiealpha.serveblog.net/api';
-// export const MEDIA_BASE_URL = 'https://scribbiealpha.serveblog.net';
+export const API_BASE_URL = 'https://scribbiealpha.serveblog.net/api';
+export const MEDIA_BASE_URL = 'https://scribbiealpha.serveblog.net';
 
 // export const API_BASE_URL = 'https://trialrender-production.up.railway.app/api';
 // export const MEDIA_BASE_URL = 'https://trialrender-production.up.railway.app/';
